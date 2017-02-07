9111d4f965ba0edf81c743c8d60f42eae52556ef
Aneesh Kohli <aneeshkohli4@gmail.com>
2017-02-07 21:07:14 +0000

    check

---
+++ Practise4.2/src/com/Prac42/controllers/Home.java
@@ -0,0 +1,12 @@
+package com.Prac42.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Home {
+	@RequestMapping("/")
+	public String showHome(){
+		return "Home";
+	}
+}
